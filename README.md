ea72842370dad8caf1f537c893c1a7fa380a01b0
Konstantin Muzyka <kosatkinms@mail.ru>
2022-03-29 23:00:29 +0300

    create new project

---
+++ README.md
@@ -0,0 +1,5 @@
+# In-Memory Sorting
+
+[![Build status](https://ci.appveyor.com/api/projects/status/2sn4emta75gmfu5x?svg=true)](https://ci.appveyor.com/project/Kosatos/ahj-dom-in-memory-sorting)
+
+[GitHub Pages](https://kosatos.github.io/ahj-dom-in-memory-sorting/)
---
+++ src/js/app.js
@@ -0,0 +1,149 @@
+const table = document.getElementById('table');
+const header = ['id', 'title', 'imdb', 'year'];
+
+const library = [
+  {
+    id: 26,
+    title: 'Побег из Шоушенка',
+    imdb: 9.3,
+    year: 1994,
+  },
+  {
+    id: 25,
+    title: 'Крёстный отец',
+    imdb: 9.2,
+    year: 1972,
+  },
+  {
+    id: 27,
+    title: 'Крёстный отец 2',
+    imdb: 9.0,
+    year: 1974,
+  },
+  {
+    id: 1047,
+    title: 'Тёмный рыцарь',
+    imdb: 9.0,
+    year: 2008,
+  },
+  {
+    id: 223,
+    title: 'Криминальное чтиво',
+    imdb: 8.9,
+    year: 1994,
+  },
+];
+
+function renderTable() {
+  document.querySelectorAll('tr').forEach((tr) => {
+    tr.remove();
+  });
+
+  let headerTr = document.createElement('tr');
+  header.forEach((item) => {
+    let headerTd = document.createElement('td');
+    headerTd.textContent = `${item}`;
+    headerTr.appendChild(headerTd);
+  });
+  table.appendChild(headerTr);
+
+  library.forEach((item) => {
+    let tr = document.createElement('tr');
+    for (const prop in item) {
+      if (item.hasOwnProperty(prop)) {
+        let td = document.createElement('td');
+        if (prop === 'year') {
+          td.textContent = `(${item[prop]})`;
+        } else if (prop === 'imdb') {
+          td.textContent = `${item[prop].toFixed(2)}`;
+        } else {
+          td.textContent = `${item[prop]}`;
+        }
+
+        tr.appendChild(td);
+      }
+    }
+    table.appendChild(tr);
+  });
+}
+
+function addArrow(arrow, attr) {
+  const cells = document.querySelectorAll('td');
+  cells.forEach((cell) => {
+    if (cell.textContent === attr) {
+      arrow === 'grow'
+        ? cell.insertAdjacentText('beforeend', ' ↓')
+        : cell.insertAdjacentText('beforeend', ' ↑');
+    }
+  });
+}
+
+function sortNumbersGrow(attr) {
+  library.sort((obj1, obj2) => {
+    return obj1[attr] - obj2[attr];
+  });
+  renderTable();
+  addArrow('grow', attr);
+}
+
+function sortNumbersDecrease(attr) {
+  library.sort((obj1, obj2) => {
+    return obj2[attr] - obj1[attr];
+  });
+  renderTable();
+  addArrow('decrease', attr);
+}
+
+function sortStringsGrow(attr) {
+  library.sort((obj1, obj2) => {
+    if (obj1[attr] < obj2[attr]) return 1;
+    if (obj1[attr] > obj2[attr]) return -1;
+    return 0;
+  });
+  renderTable();
+  addArrow('grow', attr);
+}
+
+function sortStringsDecrease(attr) {
+  library.sort((obj1, obj2) => {
+    if (obj1[attr] < obj2[attr]) return -1;
+    if (obj1[attr] > obj2[attr]) return 1;
+    return 0;
+  });
+  renderTable();
+  addArrow('decrease', attr);
+}
+
+const sortings = [
+  sortNumbersGrow,
+  sortNumbersDecrease,
+  sortStringsGrow,
+  sortStringsDecrease,
+  sortNumbersGrow,
+  sortNumbersDecrease,
+  sortNumbersGrow,
+  sortNumbersDecrease,
+];
+
+const sortingsAttr = [
+  'id',
+  'id',
+  'title',
+  'title',
+  'imdb',
+  'imdb',
+  'year',
+  'year',
+];
+
+let counter = 0;
+
+window.onload = () => {
+  setInterval(() => {
+    if (counter > sortings.length - 1) {
+      counter = 0;
+    }
+    sortings[counter](sortingsAttr[counter]);
+    counter++;
+  }, 2000);
+};
